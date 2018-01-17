66ae9bf841394e87fbcf7a5d4873188fb11e8e1f
JessWalters <jessw@iastate.edu>
2018-01-17 02:26:15 -0600

    Added Hikari connection pool

---
+++ src/main/java/com/bot/db/ConnectionPool.java
@@ -0,0 +1,39 @@
+package com.bot.db;
+
+import com.bot.Config;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import javax.sql.DataSource;
+
+public class ConnectionPool {
+	private HikariConfig hikariConfig;
+	private HikariDataSource dataSource;
+	private static ConnectionPool connectionPool;
+
+	private ConnectionPool() {
+		Config config = Config.getInstance();
+
+		hikariConfig = new HikariConfig();
+		hikariConfig.setJdbcUrl(config.getConfig("jdbc:mysql://" + Config.DB_URI + "/" + config.getConfig(Config.DB_SCHEMA)));
+		hikariConfig.setUsername(config.getConfig(Config.DB_USERNAME));
+		hikariConfig.setPassword(config.getConfig(Config.DB_PASSWORD));
+		hikariConfig.addDataSourceProperty("cachePrepStmts", "true");
+		hikariConfig.addDataSourceProperty("prepStmtCacheSize", "250");
+		hikariConfig.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+		hikariConfig.addDataSourceProperty("useServerPrepStmts", "true");
+
+		this.dataSource = new HikariDataSource(hikariConfig);
+	}
+
+	public static ConnectionPool getInstance() {
+		if (connectionPool == null) {
+			connectionPool = new ConnectionPool();
+		}
+		return connectionPool;
+	}
+
+	public DataSource getDataSource() {
+		return this.dataSource;
+	}
+}
