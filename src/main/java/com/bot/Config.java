72114a9a0ad7deae56a4bd031eab5e5a6ab3a0ff
JessWalters <jessw@iastate.edu>
2018-01-16 23:59:47 -0600

    Added hikari to pom, changed some config stuff

--- src/main/java/com/bot/Config.java
+++ src/main/java/com/bot/Config.java
@@ -11,23 +11,22 @@ public class Config {
     private final File configFile;
     private HashMap<String, String> configs;
 
-    public static final String DISCORD_TOKEN = "Discord";
-    public static final String REDDIT_TOKEN = "Reddit";
-    public static final String OWNER_ID = "Owner_Id";
-    public static final String DISCORD_BOT_ID = "Bot_Id";
-    public static final String BOT_ID = "Bot_Id";
-    public static final String BOTLIST_API_TOKEN = "BotList_API";
-    public static final String BOT_API_TOKEN = "Bot_API";
+    public static final String DISCORD_TOKEN = "DISCORD_TOKEN";
+    public static final String REDDIT_TOKEN = "REDDIT_TOKEN";
+    public static final String OWNER_ID = "OWNER_ID";
+    public static final String DISCORD_BOT_ID = "BOT_ID";
+    public static final String BOTLIST_API_TOKEN = "BOTLIST_API";
+    public static final String BOT_API_TOKEN = "BOT_PW_API";
 
 
-    public static final String USE_DB = "Use_DB";
+    public static final String USE_DB = "USE_DB";
     public static final String DB_URI = "DB_URI";
-    public static final String DB_USERNAME = "DB_Username";
-    public static final String DB_PASSWORD = "DB_Password";
-    public static final String DB_SCHEMA = "DB_Schema";
+    public static final String DB_USERNAME = "DB_USERNAME";
+    public static final String DB_PASSWORD = "DB_PASSWORD";
+    public static final String DB_SCHEMA = "DB_SCHEMA";
 
-    public static final String NUM_SHARDS = "Num_Shards";
-    public static final String PREFIX = "Prefix";
+    public static final String NUM_SHARDS = "NUM_SHARDS";
+    public static final String PREFIX = "PREFIX";
 
 
     private Config() {
