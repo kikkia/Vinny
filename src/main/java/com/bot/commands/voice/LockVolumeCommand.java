91ca5e48ff01bd16e4969e590b2b8a3ddf529f6c
JessWalters <jessw@iastate.edu>
2019-10-23 19:37:20 -0500

    add volume lock

---
+++ src/main/java/com/bot/commands/voice/LockVolumeCommand.java
@@ -0,0 +1,28 @@
+package com.bot.commands.voice;
+
+import com.bot.commands.ModerationCommand;
+import com.bot.voice.VoiceSendHandler;
+import com.jagrosh.jdautilities.command.CommandEvent;
+
+public class LockVolumeCommand extends ModerationCommand {
+
+    public LockVolumeCommand() {
+        this.name = "lockvolume";
+        this.aliases = new String[]{"lvolume", "lvol", "lockv"};
+        this.help = "Locks the volume for the playing stream. (Mod required)";
+    }
+
+    @Override
+    protected void executeCommand(CommandEvent commandEvent) {
+        VoiceSendHandler handler = (VoiceSendHandler) commandEvent.getGuild().getAudioManager().getSendingHandler();
+        if (handler == null) {
+            commandEvent.replyWarning("I am not connected to a voice channel.");
+            return;
+        }
+
+        String message = handler.toggleVolumeLock() ? "Volume is now locked" : "Volume is now unlocked";
+
+        commandEvent.replySuccess(message);
+    }
+
+}
