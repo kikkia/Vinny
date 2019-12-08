8c98237e0069b915e4e375e9a680d5731b1b4fcc
JessWalters <jessw@iastate.edu>
2019-12-08 17:49:43 -0600

    scheduled commands fix

--- src/main/java/com/bot/tasks/RunScheduledCommandsDefferedTask.kt
+++ src/main/java/com/bot/tasks/RunScheduledCommandsDefferedTask.kt
@@ -8,6 +8,7 @@ import com.bot.utils.FormattingUtils
 import com.bot.utils.Logger
 import com.bot.utils.ScheduledCommandUtils
 import net.dv8tion.jda.api.JDA
+import org.slf4j.MDC
 import java.util.concurrent.TimeUnit
 
 class RunScheduledCommandsDefferedTask() : Thread() {
@@ -22,24 +23,29 @@ class RunScheduledCommandsDefferedTask() : Thread() {
         val scheduledCommands : List<ScheduledCommand> = scheduledCommandDAO.allScheduledCommands
 
         for (sCommand in scheduledCommands) {
-            val shouldRun = System.currentTimeMillis() > sCommand.lastRun + sCommand.interval
+            try {
+                val shouldRun = System.currentTimeMillis() > sCommand.lastRun + sCommand.interval
 
-            if (shouldRun) {
-                val jda : JDA = ScheduledCommandUtils.getShardForCommand(sCommand)
-                if (jda == null) {
-                    logger.warning("Scheduled command guild not found in shards")
-                    // We dont want to remove it in case vinny is running on multiple hosts/containers
-                    continue
+                if (shouldRun) {
+                    val jda: JDA = ScheduledCommandUtils.getShardForCommand(sCommand)
+                    if (jda == null) {
+                        logger.warning("Scheduled command guild not found in shards")
+                        // We dont want to remove it in case vinny is running on multiple hosts/containers
+                        continue
+                    }
+                    val event = ScheduledCommandUtils.generateSimulatedMessageRecievedEvent(sCommand, jda)
+                    val client = ShardingManager.getInstance().commandClientImpl
+                    client.onEvent(event)
+
+                    event.channel.sendMessage("> Command scheduled by " +
+                            FormattingUtils.getUserNameOrId(sCommand.guild, sCommand.author)).queueAfter(2, TimeUnit.SECONDS)
+
+                    scheduledCommandDAO.updateLastRun(sCommand.id)
+                    metrics.markScheduledCommandRan(sCommand)
                 }
-                val event = ScheduledCommandUtils.generateSimulatedMessageRecievedEvent(sCommand, jda)
-                val client = ShardingManager.getInstance().commandClientImpl
-                client.onEvent(event)
-
-                event.channel.sendMessage("> Command scheduled by " +
-                        FormattingUtils.getUserNameOrId(sCommand.guild, sCommand.author)).queueAfter(2, TimeUnit.SECONDS)
-
-                scheduledCommandDAO.updateLastRun(sCommand.id)
-                metrics.markScheduledCommandRan(sCommand)
+            } catch (e: Exception) {
+                MDC.put("commandId", "" + sCommand.id)
+                logger.severe("Failed to run scheduled command.", e)
             }
         }
         logger.info("Finished scheduled command task")
