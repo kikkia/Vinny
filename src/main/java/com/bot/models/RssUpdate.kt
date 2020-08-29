4d4c3b1671edebefeea5f745d56f698e63c62e51
JessWalters <jessw@iastate.edu>
2020-08-28 22:01:52 -0500

    add twitch rss support and new rss field

--- src/main/java/com/bot/models/RssUpdate.kt
+++ src/main/java/com/bot/models/RssUpdate.kt
@@ -1,3 +1,3 @@
 package com.bot.models
 
-data class RssUpdate(val channel: String, val url: String, val provider: Int, val subject: String)
+data class RssUpdate(val channel: String, val url: String, val provider: Int, val subject: String, val displayName: String)
--- src/main/java/com/bot/utils/RssUtils.kt
+++ src/main/java/com/bot/utils/RssUtils.kt
@@ -21,6 +21,11 @@ class RssUtils {
                 return
             }
 
+            // Like a dingus sometimes the subject has some shitty prefix FIX THIS BALMERS CURVE SHIT
+            val cleanedSubject = rssUpdate.subject
+                    .replace("**VINNY**RT", "")
+                    .replace("**VINNY**Live", "")
+
             if (!channel.isNSFW &&
                     RssDAO.getInstance().getBySubjectAndProvider(rssUpdate.subject, rssUpdate.provider).nsfw) {
                 channel.sendMessage(ConstantStrings.RSS_NSFW_DENY).queue()
@@ -29,34 +34,38 @@ class RssUtils {
 
             if (!channel.guild.selfMember.hasPermission(Permission.MANAGE_WEBHOOKS)) {
                 channel.sendMessage("WARNING: I don't have the `MANAGE_WEBHOOKS` permission. Please give me this permission " +
-                        "to allow Scheduled commands and Subscriptions to work correctly")
+                        "to allow Scheduled commands and Subscriptions to work correctly").queue()
             } else {
                 val webhook = ScheduledCommandUtils.getWebhookForChannel(channel)
                 when (RssProvider.getProvider(rssUpdate.provider)) {
                     RssProvider.REDDIT -> {
-                        webhook.send(buildMessage("New post in ***${rssUpdate.subject}***" +
+                        webhook.send(buildMessage("New post in ***${rssUpdate.displayName}***" +
                                 "\nhttps://reddit.com${rssUpdate.url}", jda))
                     }
                     RssProvider.TWITTER -> {
                         val msg = if (rssUpdate.subject.startsWith("**VINNY**RT")) {
-                            "New retweet from ***${rssUpdate.subject.replace("**VINNY**RT", "")}"
+                            "New retweet from ***${rssUpdate.displayName}"
                         } else {
-                            "New tweet from ***${rssUpdate.subject.replace("@@", "@")}"
+                            "New tweet from ***${rssUpdate.displayName}"
                         }
                         webhook.send(buildMessage("$msg***\n" + rssUpdate.url, jda))
                     }
                     RssProvider.CHAN -> {
-                        webhook.send(buildMessage("New thread in ***${rssUpdate.subject}***\n" +
+                        webhook.send(buildMessage("New thread in ***${rssUpdate.displayName}***\n" +
                                 rssUpdate.url, jda))
                     }
                     RssProvider.YOUTUBE -> {
                         val msg = if (rssUpdate.subject.startsWith("**VINNY**Live")) {
-                            "***${rssUpdate.subject.replace("**VINNY**Live","")}*** has started streaming!\n" +
+                            "***${rssUpdate.displayName}*** has started streaming!\n" +
                                     rssUpdate.url
                         } else {
                             "New video posted from ***${rssUpdate.subject}***\n" +
                                     rssUpdate.url
                         }
+                        webhook.send(buildMessage(msg, jda))
+                    }
+                    RssProvider.TWITCH -> {
+                        val msg = "***${rssUpdate.displayName}*** just went live on twitch! ${rssUpdate.url}"
                         webhook.send(buildMessage(msg, jda))
                     }
                     else -> { // other
@@ -71,7 +80,8 @@ class RssUtils {
                     json.getString("channel"),
                     json.getString("url"),
                     json.getInt("provider"),
-                    json.getString("subject")
+                    json.getString("subject"),
+                    json.getString("displayName")
             )
         }
 
