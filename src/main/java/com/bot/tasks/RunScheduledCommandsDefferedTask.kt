601af22dba8d0d462a0b84a8f1b01becd06c1a8c
JessWalters <jessw@iastate.edu>
2020-10-15 00:13:45 -0500

    some changes

--- src/main/java/com/bot/tasks/RunScheduledCommandsDefferedTask.kt
+++ src/main/java/com/bot/tasks/RunScheduledCommandsDefferedTask.kt
@@ -16,48 +16,47 @@ class RunScheduledCommandsDefferedTask() : Thread() {
     private val metrics : MetricsManager = MetricsManager.getInstance()
 
     override fun run() {
-        logger.info("kicking off scheduled command run.")
+        try {
+            logger.info("kicking off scheduled command run.")
 
-        val scheduledCommands : List<ScheduledCommand> = scheduledCommandDAO.allScheduledCommands
+            val scheduledCommands: List<ScheduledCommand> = scheduledCommandDAO.allScheduledCommands
 
-        val startTime = System.currentTimeMillis()
-        var commandRanCount = 0
-        var failed_count = 0
+            val startTime = System.currentTimeMillis()
+            var commandRanCount = 0
+            var failed_count = 0
 
-        for (sCommand in scheduledCommands) {
-            try {
-                val shouldRun = System.currentTimeMillis() > sCommand.lastRun + sCommand.interval
+            for (sCommand in scheduledCommands) {
+                try {
+                    val shouldRun = System.currentTimeMillis() > sCommand.lastRun + sCommand.interval
 
-                if (shouldRun) {
-                    val jda: JDA = ScheduledCommandUtils.getShardForCommand(sCommand)
-                    if (jda == null) {
-                        logger.warning("Scheduled command guild not found in shards")
-                        // We dont want to remove it in case vinny is running on multiple hosts/containers
-                        continue
+                    if (shouldRun) {
+                        val jda: JDA = ScheduledCommandUtils.getShardForCommand(sCommand)
+                        val event = ScheduledCommandUtils.generateSimulatedMessageRecievedEvent(sCommand, jda)
+                        val client = ShardingManager.getInstance().commandClientImpl
+                        client.onEvent(event)
+
+                        // Commenting out due to feedback
+                        // event.channel.sendMessage("> Command scheduled by " +
+                        //        FormattingUtils.getUserNameOrId(sCommand.guild, sCommand.author)).queueAfter(2, TimeUnit.SECONDS)
+
+                        scheduledCommandDAO.updateLastRun(sCommand.id)
+                        metrics.markScheduledCommandRan(sCommand)
+                        commandRanCount++
                     }
-                    val event = ScheduledCommandUtils.generateSimulatedMessageRecievedEvent(sCommand, jda)
-                    val client = ShardingManager.getInstance().commandClientImpl
-                    client.onEvent(event)
-
-                    // Commenting out due to feedback
-                    // event.channel.sendMessage("> Command scheduled by " +
-                    //        FormattingUtils.getUserNameOrId(sCommand.guild, sCommand.author)).queueAfter(2, TimeUnit.SECONDS)
-
-                    scheduledCommandDAO.updateLastRun(sCommand.id)
-                    metrics.markScheduledCommandRan(sCommand)
-                    commandRanCount++
+                } catch (e: Exception) {
+                    MDC.put("commandId", "" + sCommand.id)
+                    logger.warning("Failed to run scheduled command.", e)
+                    scheduledCommandDAO.recordFailure(sCommand, e.toString())
+                    failed_count++
+                    MDC.clear()
                 }
-            } catch (e: Exception) {
-                MDC.put("commandId", "" + sCommand.id)
-                logger.warning("Failed to run scheduled command.", e)
-                scheduledCommandDAO.recordFailure(sCommand, e.toString())
-                failed_count++
-                MDC.clear()
             }
-        }
 
-        val runtime = System.currentTimeMillis() - startTime
-        scheduledCommandDAO.recordRunComplete(commandRanCount, runtime, failed_count)
-        logger.info("Finished scheduled command task. Commands run: $commandRanCount")
+            val runtime = System.currentTimeMillis() - startTime
+            scheduledCommandDAO.recordRunComplete(commandRanCount, runtime, failed_count)
+            logger.info("Finished scheduled command task. Commands run: $commandRanCount")
+        } catch (e: Exception) {
+            logger.severe("Problem in scheduled command task", e)
+        }
     }
 }
