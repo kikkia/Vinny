ab68e8486b7cfe047682e718bd0ebd26fd0335b9
JessWalters <jessw@iastate.edu>
2021-02-08 01:26:05 -0600

    scheduled commands on own queue

--- src/main/java/com/bot/tasks/CommandTaskExecutor.java
+++ src/main/java/com/bot/tasks/CommandTaskExecutor.java
@@ -5,8 +5,13 @@ import java.util.concurrent.Executors;
 
 public class CommandTaskExecutor {
     private static ExecutorService taskExecutor = Executors.newFixedThreadPool(400);
+    private static ExecutorService scheduledCommandExecutor = Executors.newFixedThreadPool(3);
 
     public static ExecutorService getTaskExecutor() {
         return taskExecutor;
+    }
+
+    public static ExecutorService getScheduledCommandExecutor() {
+        return scheduledCommandExecutor;
     }
 }
