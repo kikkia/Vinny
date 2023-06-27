8cae06f0e49ff03ceda4ed761d7a126fb823d465
kikkia <sabledawn@gmail.com>
2023-06-27 02:16:40 -0700

    remove reddit block

--- src/main/java/com/bot/commands/BaseCommand.java
+++ src/main/java/com/bot/commands/BaseCommand.java
@@ -72,19 +72,19 @@ public abstract class BaseCommand extends Command {
             return;
         }
 
-        // TEMP for reddit blackout
-        if (this.category == CommandCategories.REDDIT) {
-            if (!scheduled) {
-                commandEvent.replyWarning("Due to Reddit's insane increase in pricing on their API, meant to kill all " +
-                        "3rd party apps that use Reddit, Vinny included, Vinny is boycotting reddit from the 12th for an indefinite " +
-                        "amount of time. I am sorry for the inconvenience, but Reddit has forced 3rd party developer hands. " +
-                        "If there is no change in course of this new pricing model, Vinny like many other apps that tie " +
-                        "into Reddit will be forced to stop supporting Reddit on June 30th.\nIf this stuff passes and Vinny is locked " +
-                        "out of Reddit's API, subscriptions should still work, and I will be experimenting with ways to bring back " +
-                        "support for normal Reddit commands in any way I can. Sorry for the inconvenience.");
-            }
-            return;
-        }
+//        // TEMP for reddit blackout
+//        if (this.category == CommandCategories.REDDIT) {
+//            if (!scheduled) {
+//                commandEvent.replyWarning("Due to Reddit's insane increase in pricing on their API, meant to kill all " +
+//                        "3rd party apps that use Reddit, Vinny included, Vinny is boycotting reddit from the 12th for an indefinite " +
+//                        "amount of time. I am sorry for the inconvenience, but Reddit has forced 3rd party developer hands. " +
+//                        "If there is no change in course of this new pricing model, Vinny like many other apps that tie " +
+//                        "into Reddit will be forced to stop supporting Reddit on June 30th.\nIf this stuff passes and Vinny is locked " +
+//                        "out of Reddit's API, subscriptions should still work, and I will be experimenting with ways to bring back " +
+//                        "support for normal Reddit commands in any way I can. Sorry for the inconvenience.");
+//            }
+//            return;
+//        }
 
         // Add some details to the MDC on the thread before executing
         ExecutorService executorService = scheduled ? scheduledComamndExecutor : commandExecutors;
