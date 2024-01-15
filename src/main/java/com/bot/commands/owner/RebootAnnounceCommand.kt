647e4dc80762e7b4066945edb938bc35c44b3152
kikkia <sabledawn@gmail.com>
2024-01-15 02:19:16 -0800

    Make reboot more error tollerant

--- src/main/java/com/bot/commands/owner/RebootAnnounceCommand.kt
+++ src/main/java/com/bot/commands/owner/RebootAnnounceCommand.kt
@@ -24,8 +24,7 @@ class RebootAnnounceCommand : OwnerCommand() {
                         conn.volumeLocked,
                         ResumeAudioTrackMapper.queueToTracks(conn))
                 } catch (e: Exception) {
-                    commandEvent.replyError("Failed to store for ${conn.guild}, ${e.message}")
-                    return
+                    commandEvent.replyError("Failed to store for ${conn.guild}, $e")
                 }
                 conn.lastTextChannel!!.sendMessage("Vinny is rebooting for maintenance. I saved your queue and position in the current track. I will resume when I am done updating. If you want to follow the latest updates for Vinny, join the support server with the `~support` command. Sorry for the inconvenience.").complete()
             }
