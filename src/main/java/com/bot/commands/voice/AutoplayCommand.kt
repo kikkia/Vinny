077495c8d7d98e929e241cfa88fd5b589f2f2cd4
kikkia <sabledawn@gmail.com>
2024-03-18 00:26:20 -0700

    update messaging

--- src/main/java/com/bot/commands/voice/AutoplayCommand.kt
+++ src/main/java/com/bot/commands/voice/AutoplayCommand.kt
@@ -13,10 +13,11 @@ class AutoplayCommand : VoiceCommand() {
 
     override fun executeCommand(commandEvent: CommandEvent?) {
         if (!guildDAO.isGuildPremium(commandEvent!!.guild.id)) {
-            commandEvent.replyWarning("Autoplay cannot be enabled on this server. To enable autoplay a Vinny donor can run the ~premium command to enable premium on this server. Then autoplay can be used." +
-                    " You can donate to Vinny using the server subscriptions in the Vinny support server here: https://discord.com/invite/XMwyzxZ.\n" +
+            commandEvent.replyWarning("Autoplay cannot be enabled on this server. To enable autoplay a Vinny donor can run the `~donor` command to enable donor status on this server, then autoplay can be used." +
+                    " You can donate to Vinny using the server subscriptions in the Vinny support server. " +
                     "I try to keep all of Vinny's features free, but autoplay does rely on some platforms that charge for use. " +
-                    "The donations help cover the cost associated with hosting Vinny and features like this. Thanks for understanding.")
+                    "The donations help cover the cost associated with hosting Vinny and features like this. Thanks for understanding." +
+                    " https://discord.gg/XMwyzxZ")
             return
         }
         val conn = guildVoiceProvider.getGuildVoiceConnection(commandEvent.guild.idLong)
