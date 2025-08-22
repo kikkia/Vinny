04680c4939fbb24f73564bb2fe20c7bea5e122ad
kikkia <10608318+kikkia@users.noreply.github.com>
2025-08-23 00:36:01 +0900

    implement logout slash command

--- src/main/java/com/bot/commands/slash/voice/LogoutSlashCommand.kt
+++ src/main/java/com/bot/commands/slash/voice/LogoutSlashCommand.kt
@@ -1,8 +1,11 @@
 package com.bot.commands.slash.voice
 
 import com.bot.commands.slash.ExtSlashCommandEvent
+import com.bot.db.OauthConfigDAO
 
 class LogoutSlashCommand: VoiceSlashCommand(false) {
+    private val oauthConfigDAO = OauthConfigDAO.getInstance()
+
     init {
         this.name = "logout"
         this.help = "Removes voice account you have logged in"
@@ -10,6 +13,12 @@ class LogoutSlashCommand: VoiceSlashCommand(false) {
     }
 
     override fun runCommand(command: ExtSlashCommandEvent) {
-        TODO("Not yet implemented")
+        val config = oauthConfigDAO.getOauthConfig(command.user.id)
+        if (config == null) {
+            command.replyWarning("I could not find any logged in account for you.")
+            return
+        }
+        oauthConfigDAO.removeConfig(config.userId)
+        command.replySuccess("Successfully signed out")
     }
 }
